e746797449f9e4e0b2b9854e00b1b2d4d91dfecc
Stylianos Manousakis <stylianos.manousakis@broadcom.com>
2019-09-28 23:52:44 +0200

    Imported and Used Logback

--- src/main/java/springMVC_Maven101/StartMaven.java
+++ src/main/java/springMVC_Maven101/StartMaven.java
@@ -1,7 +1,14 @@
 package springMVC_Maven101;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class StartMaven {
+
+  private static final Logger log = LoggerFactory.getLogger(StartMaven.class);
   public static void main(String[] args) {
     System.out.println("Start up Maven!!");
+    log.info("Hello info");
+    log.debug("Hello debug");
   }
 }
